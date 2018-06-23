f261271b6963861235c3c856c43bacd9de480006
BlackWings1937 <36805728+BlackWings1937@users.noreply.github.com>
2018-06-23 18:01:11 +0800

    test
    using WavePlayer;

--- AudioAnalysis/Assets/Scripts/CanvasManager.cs
+++ AudioAnalysis/Assets/Scripts/CanvasManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Runtime.InteropServices;
-using WavePlayer;
 using System.IO;
 using System;
 using System.Threading;
